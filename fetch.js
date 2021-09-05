f39202502aea972038d0bf3866801a3eab30b785
ashdyed <ashdyed@outlook.com>
2021-09-05 10:05:01 +0800

    feat: real async fetch
    req.open("GET", url, true)

--- fetch.js
+++ fetch.js
@@ -1,16 +1,20 @@
+function readyStateOver(req) {
+    return new Promise(r => {
+        req.onreadystatechange = _ => {
+            if (req.readyState === 4 && req.status === 200)
+                r(req.responseText)
+        }
+    })
+}
+
 async function fetch(url) {
     const req = new XMLHttpRequest()
     req.open("GET", url, true)
-    req.send(null)
-    req.onreadystatechange = function () {
-        if (req.readyState === 4 && req.status === 200) {
-            return (req.responseText)
-        }
-    }
+    req.send()
+    return await readyStateOver(req);
 }
 
 (async _ => {
     const rsp1 = fetch("/"), rsp2 = fetch("/")
-    const all = Promise.all([rsp1, rsp2])
-    console.log(await all)
+    console.log(await rsp1, await rsp2)
 })()
