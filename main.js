9abb9f936526529d2bc149c21e513fd91408322f
ashdyed <ashdyed@outlook.com>
2021-09-08 23:03:15 +0800

    feat: post support
    post support

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-import {Posts} from "./posts";
+import {updateHandles} from "./dom"
+import {PostsHandle} from "./posts"
+import {PostHandle} from "./post";
 
 (async _ => {
-    console.log(await Posts("body #main .postTitle .postTitle2", "body #main .postDesc a", 1000))
+    updateHandles.push(PostHandle)
+    updateHandles.push(PostsHandle)
+    document.addEventListener("DOMNodeInserted", e => updateHandles.forEach(handle => handle(e)))
 })()
