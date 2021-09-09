2ccfaa5e006437ffd086af84361a59cc8a1b8559
ashdyed <ashdyed@outlook.com>
2021-09-09 15:08:09 +0800

    feat: router
    router

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-import {updateHandles} from "./dom"
-import {PostHandle, PostsHandle} from "./posts"
+import {updateHandles} from "./dom";
+import {CategoriesAndArchivesHandle, IndexHandle, PAndTagHandle, PostHandle} from "./posts";
 
 (async _ => {
-    updateHandles.push(PostsHandle)
-    updateHandles.push(PostHandle)
-    document.addEventListener("DOMNodeInserted", e => updateHandles.forEach(handle => handle(e)))
+    const paths = window.location.pathname.split("/").filter(e => e !== '')
+    while (paths.shift() !== currentBlogApp && paths.length !== 0) {
+    }
+    paths.push("")
+    switch (paths[0]) {
+        case "tag":
+            if (paths.shift()) updateHandles.push(PAndTagHandle)
+            document.addEventListener("DOMNodeInserted", e => updateHandles.forEach(handle => handle(e)))
+            break
+        case "p":
+            if (paths.shift()) updateHandles.push(PostHandle)
+            if (!paths.shift()) updateHandles.push(PAndTagHandle)
+            document.addEventListener("DOMNodeInserted", e => updateHandles.forEach(handle => handle(e)))
+            break
+        case "category":
+            updateHandles.push(CategoriesAndArchivesHandle)
+            document.addEventListener("DOMNodeInserted", e => updateHandles.forEach(handle => handle(e)))
+            break
+        case "archive":
+            updateHandles.push(CategoriesAndArchivesHandle)
+            document.addEventListener("DOMNodeInserted", e => updateHandles.forEach(handle => handle(e)))
+            break
+        default:
+            updateHandles.push(IndexHandle)
+            document.addEventListener("DOMNodeInserted", e => updateHandles.forEach(handle => handle(e)))
+    }
 })()
--- posts.js
+++ posts.js
@@ -4,12 +4,6 @@ import {IsLock, IsSign, Lock, Sign, Unlock} from "./register";
 
 const topTitle = "[置顶]"
 const postDistinct = true
-const posts = []
-
-const postsLock = "_posts"
-const postsSign = "posts"
-const postLock = "_post"
-const postSign = "post"
 
 function postsFetch(url) {
     return Fetch(url, async rsp => {
@@ -20,7 +14,7 @@ function postsFetch(url) {
         const _date = rsp.match(/<span id="post-date">([\s\S]*?)<\/span>/)
         const _readCnt = rsp.match(/<span id="post_view_count">([\s\S]*?)<\/span>/)
         const _commentCnt = rsp.match(/<span id="post_comment_count">([\s\S]*?)<\/span>/)
-        const desc = _desc[1].trim().replace(/\n/g, "")
+        const desc = _desc[1].trim()
         const date = new Date(_date[1].trim()).getTime()
         const readCnt = _readCnt[1].trim()
         const commentCnt = _commentCnt[1].trim()
@@ -73,32 +67,87 @@ export async function Posts(postEle, editEle, timeout) {
         postsMap[info.url] = {...postsMap[info.url], ...info}
     })
     const byUnix = (p, n) => n.date - p.date
-    posts.push(
+    return [
         ...Object.values(postsMap).filter(e => isTop(e)).sort(byUnix),
         ...Object.values(postsMap).filter(e => !isTop(e)).sort(byUnix)
-    )
+    ]
 }
 
-export async function PostsHandle() {
+export async function PostsHandle(flag, post, edit) {
+    Lock(`_${flag}`)
+    const posts = await Posts(post, edit, 1000);
+    Sign(flag)
+    Unlock(`_${flag}`)
+    return posts
+}
+
+export async function IndexHandle() {
+    const flag = "posts"
     const postEle = Array.from(document.querySelectorAll("body #main .postTitle .postTitle2"))
     const editEle = Array.from(document.querySelectorAll("body #main .postDesc a"))
-    if (IsSign(postsSign) || IsLock(postsLock) || postEle.length === 0 || editEle.length === 0)
+    if (IsSign(flag) || IsLock(`_${flag}`) || postEle.length === 0 || editEle.length === 0)
         return
-    Lock(postsLock)
-    await Posts(postEle, editEle, 1000)
-    Sign(postsSign)
-    Unlock(postsLock)
-    console.debug(postsSign, posts)
+    const posts = await PostsHandle(flag, postEle, editEle)
+    console.info(flag, posts)
+    IndexDOM(posts)
+}
+
+function IndexDOM(posts) {
+    const body = document.querySelector("body")
+    const table = document.createElement("table")
+    const tbody = document.createElement("tbody")
+    body.innerHTML += `<h1>欢迎访问冉尘的博客</h1>`
+    tbody.innerHTML += `<tr><th>标题</th><th>更新时间</th><th>描述</th><th>标签</th><th>分类</th></tr>`
+    posts.forEach(e => {
+        let tags = ''
+        e.tags.forEach(tag => {
+            tags += `<a href="${tag.url}">${tag.name}</a>`
+        })
+        let categories = ''
+        e.categories.forEach(category => {
+            categories += `<a href="${category.url}">${category.name}</a>`
+        })
+        tbody.innerHTML += `<tr><td><a href="${e.url}">${e.title}</a></td> <td>${new Date(e.date)}</td> <td>${e.desc}</td> <td>${tags}</td> <td>${categories}</td> </tr>`
+    })
+    table.appendChild(tbody)
+    table.setAttribute("border", "1")
+    body.appendChild(table)
 }
 
 export async function PostHandle() {
+    const flag = "post"
     const postEle = Array.from(document.querySelectorAll("body #main #post_detail .postTitle .postTitle2"))
     const editEle = Array.from(document.querySelectorAll("body #main #post_detail .postDesc a")).filter(e => e.innerText === "编辑")
-    if (IsSign(postSign) || IsLock(postLock) || postEle.length === 0 || editEle.length === 0)
+    const contentEle = Array.from(document.querySelectorAll("body #main #post_detail #cnblogs_post_body"))
+    if (IsSign(flag) || IsLock(`_${flag}`) || postEle.length === 0 || editEle.length === 0 || contentEle.length === 0)
         return
-    Lock(postLock)
-    await Posts(postEle, editEle, 1000)
-    Sign(postSign)
-    Unlock(postLock)
-    console.debug(postSign, posts)
+    const posts = await PostsHandle(flag, postEle, editEle)
+    posts[0] = {...posts[0], content: contentEle[0].innerHTML.trim()}
+    console.info(flag, posts[0])
+    PostDOM(posts[0])
+}
+
+function PostDOM(post) {
+    const body = document.querySelector("body")
+    body.innerHTML += `<div><a href="${post.url}">${post.title}</a><div>${post.content}</div></div>`
+}
+
+export async function PAndTagHandle() {
+    const flag = "posts"
+    const postEle = Array.from(document.querySelectorAll("body #main .PostList .vertical-middle"))
+    const editEle = Array.from(document.querySelectorAll("body #main .PostList .postDesc2 a"))
+    if (IsSign(flag) || IsLock(`_${flag}`) || postEle.length === 0 || editEle.length === 0)
+        return
+    const posts = await PostsHandle(flag, postEle, editEle);
+    console.info(flag, posts)
+}
+
+export async function CategoriesAndArchivesHandle() {
+    const flag = "posts"
+    const postEle = Array.from(document.querySelectorAll("body #main .entrylistItem .entrylistItemTitle"))
+    const editEle = Array.from(document.querySelectorAll("body #main .entrylistItem .entrylistItemPostDesc a")).filter(e => e.innerText === "编辑")
+    if (IsSign(flag) || IsLock(`_${flag}`) || postEle.length === 0 || editEle.length === 0)
+        return
+    const posts = await PostsHandle(flag, postEle, editEle);
+    console.info(flag, posts)
 }
