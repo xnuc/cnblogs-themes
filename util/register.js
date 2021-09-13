5f7e2993e113d51ef4deab6eb954cd950c38644a
zeebeesong <zeebeesong@tencent.com>
2021-09-14 00:03:25 +0800

    feat: all pages support
    all pages support

--- util/register.js
+++ util/register.js
@@ -2,21 +2,21 @@ const locked = "locked"
 const loaded = "loaded"
 
 export function Lock(lock) {
-    document.querySelector("body").setAttribute(lock, locked)
+    document.querySelector("head").setAttribute(lock, locked)
 }
 
 export function Unlock(lock) {
-    document.querySelector("body").removeAttribute(lock)
+    document.querySelector("head").removeAttribute(lock)
 }
 
 export function IsLock(lock) {
-    return document.querySelector("body").getAttribute(lock) === locked
+    return document.querySelector("head").getAttribute(lock) === locked
 }
 
 export function Sign(sign) {
-    document.querySelector("body").setAttribute(sign, loaded)
+    document.querySelector("head").setAttribute(sign, loaded)
 }
 
 export function IsSign(sign) {
-    return document.querySelector("body").getAttribute(sign) === loaded
+    return document.querySelector("head").getAttribute(sign) === loaded
 }
